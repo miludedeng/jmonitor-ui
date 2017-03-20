d849f0bc312050df4ee5e6c164aacfe91d07d69f
huwei <hw@zving.com>
2017-03-20 19:12:46 +0800

    init

---
+++ src/components/Welcome.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="welcome">aaa
+    <div id="page-wrapper-begin" class="page-wrapper">
+        <div class="begin-panel"><i class="glyphicon glyphicon-hand-left"></i> 请点击左侧应用 </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+</style>
